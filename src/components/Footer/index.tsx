7318b5462cb8b1098a65dc4d15aeb5969a509b03
Lara Krefski <laauster@gmail.com>
2026-04-21 21:36:26 -0400

    Added link for GitHub to the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ export function Footer() {
         </p>
         <div className="flex space-x-6 mt-4 md:mt-0">
           <a href="https://www.linkedin.com/in/lara-krefski-3788371/" target="_blank" className="text-[#141e22] hover:opacity-80 transition-colors transition-opacity">LinkedIn</a>
-          <a href="#" target="_blank" className="text-[#141e22] hover:opacity-80 transition-colors transition-opacity">GitHub</a>
+          <a href="https://github.com/larakrefski" target="_blank" className="text-[#141e22] hover:opacity-80 transition-colors transition-opacity">GitHub</a>
         </div>
       </div>
     </footer>
